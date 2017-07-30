2eda282fa24c03e98d760eb56fe5d02f090cd617
Evangelos Channakis <vanchann@gmail.com>
2017-07-30 17:20:48 +0300

    Changed indentation from tabs to 4 spaces for better presentation.

--- intcheck.sh
+++ intcheck.sh
@@ -8,23 +8,23 @@ method=md5sum
 # Process options
 while getopts :m: opt
 do
-	case $opt in
-		m) method=$OPTARG;;
-		*) echo "Unknown option: $opt";;
-	esac
+    case $opt in
+        m) method=$OPTARG;;
+        *) echo "Unknown option: $opt";;
+    esac
 done
 # Shift to parameters
 shift $[ $OPTIND - 1 ]
 
 # Calculate hash and check the result
 if [ $# -eq 2 ]
-	then
-		testing_sum=(`$method $1`)
-		if [[ $testing_sum == $2 ]]
-			then echo OK - $testing_sum
-			else echo ERROR - $testing_sum
-		fi
-	else
-		echo Usage: `basename $0` [-m method] filename hashvalue
-		echo Default method md5sum
+    then
+        testing_sum=(`$method $1`)
+        if [[ $testing_sum == $2 ]]
+            then echo OK - $testing_sum
+            else echo ERROR - $testing_sum
+        fi
+    else
+        echo Usage: `basename $0` [-m method] filename hashvalue
+        echo Default method md5sum
 fi
